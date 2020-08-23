924d087fee2584117a3d063a2b0a4d734e8b83d3
Victor Banerjee <recervictory@gmail.com>
2020-08-23 19:17:26 +0530

    Counting DNA Nucleotides

---
+++ 001_DNA.py
@@ -0,0 +1,18 @@
+'''
+A solution to a ROSALIND bioinformatics problem.
+Problem Title: Counting DNA Nucleotides
+Rosalind ID: DNA
+Rosalind #: 001
+URL: http://rosalind.info/problems/dna/
+'''
+
+with open('data/rosalind_dna.txt') as input_data:
+	dna = input_data.read()
+
+nuc_count = []
+for nucleotide in ['A', 'C', 'G', 'T']:
+	nuc_count.append(str(dna.count(nucleotide)))
+
+print(' '.join(nuc_count))
+with open('output/001_DNA.txt', 'w') as output_data:
+	output_data.write(' '.join(nuc_count))
